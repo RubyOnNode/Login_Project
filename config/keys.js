227208234615e49cb449f0f2cd166407d11f9153
Hitesh Meena <68632681+RubyOnNode@users.noreply.github.com>
2020-08-25 02:18:35 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dbPassword = 'Your mongo uri';
 
 module.exports = {
     mongoURI: dbPassword
